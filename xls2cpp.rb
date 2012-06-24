5d36cdd1fa051cd07aebd0d65cd2bd83ae28e601
hirax <jun@hirax.net>
2012-06-24 09:43:55 +0900

    名前対応（前書いたソースをマージ）

--- xls2cpp.rb
+++ xls2cpp.rb
@@ -1,56 +1,67 @@
 # ruby -w xls2cpp.rb sample.xls > sample.cpp
 # cl /EHsc /Ox sample.cpp
-# jun hirabayashi jun@hirax.net
-# 2012.06.23
+# jun hirabayashi jun@hirax.net　2009/12/19-
+# 2012.06.23rev.
 
 
 require 'win32ole'
 
 def getAbsolutePath(filename)
-  fso=WIN32OLE.new('Scripting.FileSystemObject')
-  fso.GetAbsolutePathName(filename)
+    fso=WIN32OLE.new('Scripting.FileSystemObject')
+    fso.GetAbsolutePathName(filename)
 end
 
 def getAlphabet(n)
-  val=n.to_f/25.0 
-  mod=n%25
-  result=''
-  result+=('A'..'Z').to_a[(n-val)/25] if val>1
-  result+=('A'..'Z').to_a[mod]
+    val=n.to_f/25.0 
+    mod=n%25
+    result=''
+    result+=('A'..'Z').to_a[(n-val)/25] if val>1
+    result+=('A'..'Z').to_a[mod]
 end
 
 def defFunc(state,book)
-  src=''
-  book.Worksheets.each do |sheet|
-    y=1
-    sheet.UsedRange.Rows.each do |row|
-      x=0
-      record=[]
-      row.Columns.each do |cell|
-        if state==:def && cell.Value.to_s!=''
-          record<<'  float '+getAlphabet(x)+y.to_s+'=0.;'
+    names={}
+	book.names.each do |name|
+		cell=$1.gsub('$','') if /!([^!]+)$/=~name.RefersTo
+        names[cell]=name.name
+	end
+    src=''
+    book.Worksheets.each do |sheet|
+        y=1
+        sheet.UsedRange.Rows.each do |row|
+            x=0
+            record=[]
+            row.Columns.each do |cell|
+                name=getAlphabet(x)+y.to_s
+                name=names[name] if names[name]
+                
+                begin
+                    rescue
+                end
+                if state==:def &&cell.Value.to_s!=''
+                    record<<'  float '+name+'=0.;'
+                end
+                if state==:init &&cell.Value!=''
+                    record<<'  '+name+
+                    '='+cell.Value.to_s+';' if cell.Value.to_s!=''
+                end
+                if state==:calc &&cell.Formula!=''
+                    t=cell.Formula.sub(/[=$]/,'')
+                    record<<'  '+name+'='+t+';'
+                end
+                if state==:display && cell.Formula!=''
+                    record << '    cout << '+'"'+getAlphabet(x) + y.to_s+' = " << '+ getAlphabet(x) + y.to_s+' << endl;'
+                end
+                x+=1
+            end
+            if record.join('').gsub("\n",'')!=''
+                src+=record.join("\n")+"\n" 
+            end
+            y+=1
         end
-        if state==:init && cell.Value!=''
-          record<<'  '+getAlphabet(x)+y.to_s+
-            '='+cell.Value.to_s+';' if cell.Value.to_s!=''
-        end
-        if state==:calc && cell.Formula!=''
-          t=cell.Formula.sub(/[=$]/,'')
-          record<<'  '+getAlphabet(x)+y.to_s+'='+t+';' unless /^[+-]?[0-9]*[\.]?[0-9]+$/ =~ t
-        end
-        if state==:display && cell.Formula!=''
-          record << '    cout << '+'"'+getAlphabet(x) + y.to_s+' = " << '+ getAlphabet(x) + y.to_s+' << endl;'
-        end
-        x+=1
-      end
-      if record.join('').gsub("\n",'')!=''
-        src+=record.join("\n")+"\n" 
-      end
-      y+=1
     end
-  end
-  return src
-end
+    return src
+    end
 
 filename=getAbsolutePath(ARGV[0])
 excel=WIN32OLE.new('Excel.Application')
